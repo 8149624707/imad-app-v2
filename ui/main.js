1dae9365626853719224553209599da6c723d6fa
8149624707 <nirajmourya786@gmail.com>
2017-02-19 01:42:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,8 @@
 console.log('Loaded!');
 var element = document.getElementById('main-txt');
 element.innerHTML='kiran sharma';
+var img= document.getElementById('image');
+img.onclick = function(){
+    img.style.marginLeft ='100px';
+    
+};
