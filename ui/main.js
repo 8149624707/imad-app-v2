d0aa45a416647617da11cae4926c5e3aebab7817
8149624707 <nirajmourya786@gmail.com>
2017-02-25 01:00:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -28,7 +28,7 @@ var submit =document.getElementById(`submit_btn`);
 submit.onclick=function(){
     
     var names =[`name1`,`name2`,`name3`,`name  4`];
-    var lst=``;
+    var lst='';
     for(var i=0;i<names.length;i++)
     {
         lst += '<li>'+names[i]+'</li>';
