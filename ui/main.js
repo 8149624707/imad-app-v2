9869fa8641d6b0177de0ab8a1fed3505940985d2
8149624707 <nirajmourya786@gmail.com>
2017-03-02 01:29:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -27,12 +27,27 @@ var name = nameinput.value;
 var submit =document.getElementById(`submit_btn`);
 submit.onclick= function(){
     
-    var names =[`name1`,`name2`,`name3`,`name  4`];
-    var lst='';
-    for(var i=0;i<names.length;i++)
-    {
-        lst += '<li>'+names[i]+'</li>';
-    }
-    var ul =document.getElementById(`namelist`);
-    ul.innerHTML =lst;
+    var request= new XMLHttpRequest();
+    
+    request.onreadystatechange= function(){
+        if(request.readyState === XMLHttpRequest.DONE)
+        {
+            if(request.status === 200)
+            {
+                  var names =request.responseText;
+                  names=JSON.parse(names);
+                  var lst='';
+                  for(var i=0;i<names.length;i++)
+                  {
+                          lst += '<li>'+names[i]+'</li>';
+                   }
+            var ul =document.getElementById(`namelist`);
+            ul.innerHTML =lst;
+            }
+        }
+    };
+    
+    request.open(`GET`,`http://user-8149624707.imad.hasura-app.io/submit-name?name=`+name,true);
+    request.send(null);
+  
 };
