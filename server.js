e5524a4cc8f9e05350b1b8276bba5362920a2380
8149624707 <nirajmourya786@gmail.com>
2017-03-11 12:26:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool=require('pg').Pool;
+
+var config = {
+  user : 'user-8149624707',
+  database:'user-8149624707',
+  host:'db.imad.hasura-app.io',
+  port:'5432',
+  password:process.env.DB_PASSWORD
+};
+
+
 
 var app = express();
 app.use(morgan('combined'));
@@ -8,6 +19,24 @@ var counter = 0;
 app.get('/counter',function(req,res){
     counter=counter+1;
     res.send(counter.toString());
+});
+
+// create the pool somewhere globally so its lifetime
+// lasts for as long as your app is running
+var pool = new Pool(config);
+app.get('/test-db',function(req,res){
+    //make a select request
+    //return a response with result
+
+    pool.query('se;ect 8 from test',function(err,query){
+        if(err)
+        {
+           res.status(500).send(err.toString());
+        }else
+        {
+            res.send(JSON.stringify(result));
+        }
+    });
 });
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
